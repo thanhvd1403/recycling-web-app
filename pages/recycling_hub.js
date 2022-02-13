2b6720f4619c60e056bff340e184896fcab77bf5
Phidung <dungho01092002@gmail.com>
2022-02-13 16:52:38 +0800

    Updated README || add redux, ggmap API

---
+++ pages/recycling_hub.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import { useSelector } from "react-redux";
+
+import Info from "../components/info";
+import Settings from "../components/settings";
+import GoogleMaps from "../components/google-maps";
+import NoMaps from "../components/no-maps";
+
+function selector(state) {
+  return {
+    loadScriptChecked: state.getIn(["app", "loadScriptChecked"]),
+    googleMapsApiKey: state.getIn(["app", "googleMapsApiKey"]),
+  };
+}
+
+function IndexPage() {
+  const { googleMapsApiKey, loadScriptChecked } = useSelector(selector);
+
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-lg-10 offset-lg-1">
+          <Info />
+
+          <div className="mb-5">
+            <h2 className="h5">Settings</h2>
+
+            <Settings />
+          </div>
+
+          <div className="mb-5">
+            <h2 className="h5">Examples</h2>
+
+            {loadScriptChecked && googleMapsApiKey.length >= 38 ? (
+              <GoogleMaps />
+            ) : (
+              <NoMaps />
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default React.memo(IndexPage);
