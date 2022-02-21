7859106923c53f4d96543dd4e4dfe2fee939af05
Phidung <dungho01092002@gmail.com>
2022-02-21 21:21:52 +0800

    Add scroll to top

--- components/layout.js
+++ components/layout.js
@@ -2,13 +2,14 @@ import Alert from "../components/alert";
 import Footer from "../components/footer";
 import Meta from "../components/meta";
 import NavBar from "./nav-bar";
+import AppBar from "@mui/material/ToolBar";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        {/* <Alert preview={preview} /> */}
+        <div id="back-to-top-anchor" />
         <NavBar />
         <div>
           <main>{children}</main>
--- components/nav-bar.js
+++ components/nav-bar.js
@@ -20,6 +20,47 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import Link from "next/link";
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Zoom from "@mui/material/Zoom";
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 500,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 function OnScroll(props) {
   const { children, window } = props;
@@ -192,8 +233,13 @@ export default function NavBar(props) {
       <ResponsiveAppBar />
       {/* </OnScroll> */}
 
+      <ScrollTop {...props}>
+        <Fab color="inherit" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
       <Stack sx={{ width: "100%" }} spacing={2}>
-        <Alert severity="error">This is an error alert — check it out!</Alert>
+        {/* <Alert severity="error">This is an error alert — check it out!</Alert> */}
       </Stack>
     </>
   );
