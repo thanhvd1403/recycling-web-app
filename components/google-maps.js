03c15177d80e5f322387aa1990a23b87b07dcf7f
Phidung <dungho01092002@gmail.com>
2022-03-26 09:59:41 +0800

    Googke map without database

--- components/google-maps.js
+++ components/google-maps.js
@@ -1,145 +1,278 @@
-import React, { memo } from "react";
-import uniqid from "uniqid";
-import { useSelector } from "react-redux";
+import {
+  GoogleMap,
+  LoadScript,
+  Autocomplete,
+  Marker,
+  Circle,
+} from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import {
+  Checkbox,
+  Menu,
+  Box,
+  Tooltip,
+  IconButton,
+  FormGroup,
+  FormControlLabel,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+// import { collection, getDocs, doc, deleteDoc } from "@firebase/firestore";
+// import db from "../firebase-config";
+// import { deleteAllPlaces, initializeDatabase } from "../lib/initializeDatabase";
+import { placesData } from "../data/data";
 
-import { LoadScript } from "@react-google-maps/api";
+const libraries = ["places"];
 
-import Card from "../components/card";
+const containerStyle = {
+  width: "100%",
+  height: "700px",
+  display: "block",
+};
 
-import CheckboxData from "../components/checkbox-data";
-import CheckboxDirections from "../components/checkbox-directions";
-import CheckboxHeatmap from "../components/checkbox-heatmap";
-import CheckboxTraffic from "../components/checkbox-traffic";
-import CheckboxOverlayView from "../components/checkbox-overlay-view";
-import CheckboxShapes from "../components/checkbox-shapes";
-import CheckboxDrawing from "../components/checkbox-drawing";
-import CheckboxBicycling from "../components/checkbox-bicycling";
-import CheckboxTransit from "../components/checkbox-transit";
-import CheckboxGround from "../components/checkbox-ground";
-import CheckboxOptions from "../components/checkbox-options";
-import CheckboxSearchBox from "./checkbox-search-box";
-
-import SectionData from "./section-data";
-import SectionDirections from "./section-directions";
-import SectionHeatmap from "./section-heatmap";
-import SectionTraffic from "./section-traffic";
-import SectionShapes from "./section-shapes";
-import SectionDrawing from "./section-drawing";
-import SectionBicycling from "./section-bicycling";
-import SectionTransit from "./section-transit";
-import SectionGround from "./section-ground";
-import SectionOptions from "./section-options";
-import SectionOverlayView from "./section-overlay-view";
-import SectionSearchBox from "./section-search-box";
-
-import * as styles from "./styles.module.css";
-
-const Loading = <div className={styles.loadingStyle} />;
-
-const googleMapsLibraries = ["drawing", "visualization", "places"];
-
-const loaderId = uniqid("loader-");
-
-const onLoad = () => console.log("script loaded");
-
-const onError = (err) => console.log("onError: ", err);
-
-function selector(state) {
-  return {
-    language: state.getIn(["app", "language"]),
-    googleMapsApiKey: state.getIn(["app", "googleMapsApiKey"]),
-  };
+/**
+ * @args (point1, point2): {object} { lat: number, lng: number }
+ * @returns {number} distance in meters.
+ */
+function haversine_distance(point1, point2) {
+  const R = 6371071; // Radius of the Earth in miles
+  let rlat1 = point1.lat * (Math.PI / 180); // Convert degrees to radians
+  let rlat2 = point2.lat * (Math.PI / 180); // Convert degrees to radians
+  let difflat = rlat2 - rlat1; // Radian difference (latitudes)
+  let difflon = (point2.lng - point1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+  var d =
+    2 *
+    R *
+    Math.asin(
+      Math.sqrt(
+        Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+          Math.cos(rlat1) *
+            Math.cos(rlat2) *
+            Math.sin(difflon / 2) *
+            Math.sin(difflon / 2)
+      )
+    );
+  return d;
 }
 
-function GoogleMaps() {
-  const { googleMapsApiKey, language } = useSelector(selector);
+export default function GoogleMapComponent() {
+  const [center, setCenter] = useState({
+    lat: 1.348,
+    lng: 103.683,
+  });
+  const [bounds, setbounds] = useState(1); // bounds whole singapore
+  const [boundRadius, setBoundRadius] = useState(0);
+  const [zoomLevel, setzoomLevel] = useState(12);
+  const [autocomplete, setAutocomplete] = useState(null);
+  const onLoad = (autocomplete) => {
+    setAutocomplete(autocomplete);
+  };
 
+  const [searchMarker, setSearchMarker] = useState(null);
+  const [curPlace, setCurPlace] = useState({});
+  const onPlaceChanged = () => {
+    // let input = document.getElementById("search-input");
+    let thisPlace = autocomplete.getPlace();
+    const lat = thisPlace.geometry.location.lat();
+    const lng = thisPlace.geometry.location.lng();
+    setzoomLevel(13);
+    setCenter({ lat, lng });
+    setSearchMarker({ lat, lng });
+    setCurPlace(thisPlace);
+  };
+
+  const [places, setPlaces] = useState(null);
+  useEffect(() => {
+    // const getPlaces = async () => {
+    //   const placesCollectionRef = collection(db, "places");
+    //   const data = await getDocs(placesCollectionRef);
+    //   setPlaces(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    //   console.log(places);
+    // };
+    // getPlaces();
+    setPlaces(placesData);
+  }, [places]);
+
+  const options = {
+    strictBounds: true,
+    bounds: {
+      north: center.lat + bounds,
+      south: center.lat - bounds,
+      east: center.lng + bounds,
+      west: center.lng - bounds,
+    },
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setanchorUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setanchorUser(null);
+  };
+  const [anchorUser, setanchorUser] = useState(null);
   return (
-    <LoadScript
-      id={loaderId}
-      googleMapsApiKey={googleMapsApiKey}
-      language={language}
-      region="EN"
-      version="weekly"
-      onLoad={onLoad}
-      onError={onError}
-      loadingElement={Loading}
-      libraries={googleMapsLibraries}
-      preventGoogleFontsLoading
-    >
-      <Card>
-        <CheckboxData />
-
-        <SectionData />
-      </Card>
-
-      <Card>
-        <CheckboxDirections />
-
-        <SectionDirections />
-      </Card>
-
-      <Card>
-        <CheckboxHeatmap />
-
-        <SectionHeatmap />
-      </Card>
-
-      <Card>
-        <CheckboxTraffic />
-
-        <SectionTraffic />
-      </Card>
-
-      <Card>
-        <CheckboxShapes />
-
-        <SectionShapes />
-      </Card>
-
-      <Card>
-        <CheckboxDrawing />
-
-        <SectionDrawing />
-      </Card>
-
-      <Card>
-        <CheckboxBicycling />
-
-        <SectionBicycling />
-      </Card>
-
-      <Card>
-        <CheckboxTransit />
-
-        <SectionTransit />
-      </Card>
-
-      <Card>
-        <CheckboxGround />
-
-        <SectionGround />
-      </Card>
-
-      <Card>
-        <CheckboxOptions />
-
-        <SectionOptions />
-      </Card>
-
-      <Card>
-        <CheckboxOverlayView />
-
-        <SectionOverlayView />
-      </Card>
-
-      <Card>
-        <CheckboxSearchBox />
-
-        <SectionSearchBox />
-      </Card>
-    </LoadScript>
+    <>
+      <LoadScript
+        googleMapsApiKey="AIzaSyBYqHAL0_W6xy3_FaKoxDrnpBsWNkj0urc"
+        libraries={libraries}
+      >
+        <Box sx={{ flexGrow: 0, display: "inline-flex" }}>
+          <Tooltip title="E-waste settings">
+            <IconButton
+              sx={{ p: 0, fontSize: 24 }}
+              onClick={handleOpenUserMenu}
+            >
+              Filters
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <FormGroup sx={{ ml: "10px" }}>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="ICT Equipments"
+              />
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="Batterries and Lamps"
+              />
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="Ink and toner cartridges"
+              />
+            </FormGroup>
+          </Menu>
+        </Box>
+        <Box
+          sx={{
+            minWidth: 100,
+            display: "inline-flex",
+            paddingLeft: "10px",
+            paddingTop: "20px",
+          }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id="search-radius-input">Within</InputLabel>
+            <Select
+              labelId="search-radius"
+              id="search-radius"
+              value={boundRadius}
+              label="Radius"
+              onChange={(event) => {
+                const radius = event.target.value;
+                if (radius) {
+                  setBoundRadius(radius);
+                } else {
+                  setBoundRadius(0);
+                  setbounds(1);
+                }
+              }}
+            >
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3000}>3km</MenuItem>
+              <MenuItem value={5000}>5km</MenuItem>
+              <MenuItem value={7000}>7km</MenuItem>
+              <MenuItem value={10000}>10km</MenuItem>
+              <MenuItem value={15000}>15km</MenuItem>
+              <MenuItem value={20000}>20km</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={zoomLevel}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <>
+            {places?.map((place) => {
+              const radius = boundRadius > 0 ? boundRadius : 50000;
+              if (haversine_distance(center, place.location) <= radius) {
+                console.log(place);
+                const location = place.location;
+                return (
+                  <Marker
+                    key={place.id}
+                    position={{ lat: location.lat, lng: location.lng }}
+                    title={`${place.Address}, Singapore ${place.PostalCode}`}
+                    onClick={() => {
+                      // setzoomLevel(13);
+                      // setCenter({ lat: place.lat, lng: place.lng });
+                      // setSearchMarker({ lat: location.lat, lng: location.lng });
+                      // setCurPlace(place);
+                    }}
+                    icon={"/assets/markers/greenMarkers.png"}
+                  />
+                );
+              }
+            })}
+            <Marker
+              onLoad={() => {}}
+              position={searchMarker}
+              title={`${curPlace.name} \n${curPlace.formatted_address}`}
+              editable={true}
+              onDblClick={() => {}}
+              onRightClick={() => {}}
+            />
+            <Circle
+              center={searchMarker}
+              radius={boundRadius}
+              option={{
+                strokeColor: "#F9F9F9",
+                strokeOpacity: 0.8,
+                strokeWeight: 1,
+                fillColor: "#F9F9F9",
+                fillOpacity: 0.1,
+              }}
+            />
+            <Autocomplete
+              onLoad={onLoad}
+              onPlaceChanged={onPlaceChanged}
+              options={options} // further restriction by radius bounds.
+              restrictions={{ country: ["sg"] }} // restrict to singapore
+              id="autocomplete"
+              types={["geocode"]}
+            >
+              <input
+                type="text"
+                id="search-input"
+                placeholder="Search for E-waste"
+                style={{
+                  boxSizing: `border-box`,
+                  border: `1px solid transparent`,
+                  width: `240px`,
+                  height: `32px`,
+                  padding: `0 12px`,
+                  borderRadius: `3px`,
+                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                  fontSize: `14px`,
+                  outline: `none`,
+                  textOverflow: `ellipses`,
+                  position: "absolute",
+                  left: "25%",
+                  marginLeft: "-150px",
+                }}
+              />
+            </Autocomplete>
+          </>
+        </GoogleMap>
+      </LoadScript>
+    </>
   );
 }
-
-export default memo(GoogleMaps);
--- pages/recycling_page.js
+++ pages/recycling_page.js
@@ -1,254 +1,8 @@
 import Container from "../components/container";
 import Layout from "../components/layout";
 import Head from "next/head";
-import {
-  GoogleMap,
-  LoadScript,
-  Autocomplete,
-  Marker,
-  Circle,
-} from "@react-google-maps/api";
-import React, { useState, useEffect } from "react";
-import {
-  Checkbox,
-  Menu,
-  Box,
-  Tooltip,
-  IconButton,
-  FormGroup,
-  FormControlLabel,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
-import { deletePlace, getAllPlaces, getPlace } from "../lib/ewaste_api";
-
-const bounds_arc_len = {
-  3000: 0.027,
-  5000: 0.045,
-  7000: 0.063,
-  10000: 0.09,
-  15000: 0.135,
-  20000: 0.18,
-};
-const libraries = ["places"];
-
-const containerStyle = {
-  width: "100%",
-  height: "700px",
-  display: "block",
-};
-
-// initializeDatabase();
-
-function GoogleMapComponent() {
-  const [center, setCenter] = useState({
-    lat: 1.348,
-    lng: 103.683,
-  });
-  const [bounds, setbounds] = useState(1); // bounds whole singapore
-  const [boundRadius, setBoundRadius] = useState(0);
-  const [zoomLevel, setzoomLevel] = useState(12);
-  const [autocomplete, setAutocomplete] = useState(null);
-  const onLoad = (autocomplete) => {
-    setAutocomplete(autocomplete);
-  };
-
-  const [searchMarker, setSearchMarker] = useState(null);
-  const [curPlace, setCurPlace] = useState({});
-  const [zoomIn, setZoomIn] = useState(0);
-  const onPlaceChanged = () => {
-    // let input = document.getElementById("search-input");
-    let thisPlace = autocomplete.getPlace();
-    const lat = thisPlace.geometry.location.lat();
-    const lng = thisPlace.geometry.location.lng();
-    setzoomLevel(13);
-    setCenter({ lat, lng });
-    setSearchMarker({ lat, lng });
-    setCurPlace(thisPlace);
-  };
-
-  const [places, setPlaces] = useState(null);
-  useEffect(() => {
-    // getAllPlaces()
-    //   .then((data) => {
-    //     setPlaces(data);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  }, [places]);
-
-  const options = {
-    strictBounds: true,
-    bounds: {
-      north: center.lat + bounds,
-      south: center.lat - bounds,
-      east: center.lng + bounds,
-      west: center.lng - bounds,
-    },
-  };
-
-  const handleOpenUserMenu = (event) => {
-    setanchorUser(event.currentTarget);
-  };
-  const handleCloseUserMenu = () => {
-    setanchorUser(null);
-  };
-  const [anchorUser, setanchorUser] = useState(null);
-  return (
-    <>
-      <LoadScript
-        googleMapsApiKey="AIzaSyBYqHAL0_W6xy3_FaKoxDrnpBsWNkj0urc"
-        libraries={libraries}
-      >
-        <Box sx={{ flexGrow: 0, display: "inline-flex" }}>
-          <Tooltip title="E-waste settings">
-            <IconButton
-              sx={{ p: 0, fontSize: 24 }}
-              onClick={handleOpenUserMenu}
-            >
-              Filters
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: "45px" }}
-            id="menu-appbar"
-            anchorEl={anchorUser}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={Boolean(anchorUser)}
-            onClose={handleCloseUserMenu}
-          >
-            <FormGroup sx={{ ml: "10px" }}>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="ICT Equipments"
-              />
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="Batterries and Lamps"
-              />
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="Ink and toner cartridges"
-              />
-            </FormGroup>
-          </Menu>
-        </Box>
-        <Box
-          sx={{
-            minWidth: 100,
-            display: "inline-flex",
-            paddingLeft: "10px",
-            paddingTop: "20px",
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel id="search-radius-input">Within</InputLabel>
-            <Select
-              labelId="search-radius"
-              id="search-radius"
-              value={boundRadius}
-              label="Radius"
-              onChange={(event) => {
-                const radius = event.target.value;
-                if (radius) {
-                  setBoundRadius(radius);
-                  // setbounds(bounds_arc_len[radius]);
-                } else {
-                  setBoundRadius(0);
-                  setbounds(1);
-                }
-                console.log(places);
-              }}
-            >
-              <MenuItem value={0}>All</MenuItem>
-              <MenuItem value={3000}>3km</MenuItem>
-              <MenuItem value={5000}>5km</MenuItem>
-              <MenuItem value={7000}>7km</MenuItem>
-              <MenuItem value={10000}>10km</MenuItem>
-              <MenuItem value={15000}>15km</MenuItem>
-              <MenuItem value={20000}>20km</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={zoomLevel}
-        >
-          {/* Child components, such as markers, info windows, etc. */}
-          <>
-            <Marker
-              onLoad={() => {}}
-              position={searchMarker}
-              title={`${curPlace.name} \n${curPlace.formatted_address}`}
-              onClick={() => {
-                setzoomLevel(zoomLevel + 1);
-                setZoomIn(zoomIn + 1);
-              }}
-              onRightClick={() => {
-                setzoomLevel(zoomLevel - zoomIn);
-                setZoomIn(0);
-              }}
-              icon="../public/assets/markers/green_markers.png"
-            />
-
-            <Circle
-              center={searchMarker}
-              radius={boundRadius}
-              option={{
-                strokeColor: "#FF0000",
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#FF0000",
-                fillOpacity: 0.35,
-              }}
-            />
-            <Autocomplete
-              onLoad={onLoad}
-              onPlaceChanged={onPlaceChanged}
-              options={options} // further restriction by radius bounds.
-              restrictions={{ country: ["sg"] }} // restrict to singapore
-              id="autocomplete"
-              types={["geocode"]}
-            >
-              <input
-                type="text"
-                id="search-input"
-                placeholder="Search for E-waste"
-                style={{
-                  boxSizing: `border-box`,
-                  border: `1px solid transparent`,
-                  width: `240px`,
-                  height: `32px`,
-                  padding: `0 12px`,
-                  borderRadius: `3px`,
-                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                  fontSize: `14px`,
-                  outline: `none`,
-                  textOverflow: `ellipses`,
-                  position: "absolute",
-                  left: "25%",
-                  marginLeft: "-150px",
-                }}
-              />
-            </Autocomplete>
-          </>
-        </GoogleMap>
-      </LoadScript>
-    </>
-  );
-}
+import GoogleMapComponent from "../components/google-maps";
+// import { getAllPosts } from "../lib/api";
 
 export default function RecyclePage() {
   return (
@@ -264,4 +18,19 @@ export default function RecyclePage() {
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  // await deleteAllPlaces();
+  // const allPosts = getAllPosts([
+  //   "title",
+  //   "date",
+  //   "slug",
+  //   "author",
+  //   "coverImage",
+  //   "excerpt",
+  // ]);
+  return {
+    props: { 1: 2 },
+  };
 }
