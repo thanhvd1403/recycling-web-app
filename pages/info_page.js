9eeacf7c8e4a0ba939487c718e5112071e31b710
Vu Duc Anh <67143004+vuducanh0802@users.noreply.github.com>
2022-03-27 14:48:11 +0800

    Update info_page.js

--- pages/info_page.js
+++ pages/info_page.js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 
 export default function InfoPage({ allPosts }) {
   const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  const morePosts = allPosts.slice(0,3);
   return (
     <>
       <Layout preview={true}>
@@ -61,68 +61,180 @@ Electronic scrap components, such as CPUs, contain potentially harmful materials
 
 
   <div id="ToE" class="flex space-x-7 py-10 pl-10 pr-10">
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-
-    
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
   </div>
 
   <div id="ToE" class="flex space-x-7 py-10 pl-10 pr-10">
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-
-    
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
   </div>
 
   <div id="ToE" class="flex space-x-7 py-10 pl-10 pr-10">
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-
-    
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
   </div>
 
   <div id="ToE" class="flex space-x-7 py-10 pl-10 pr-10">
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-    <div>
-    <Container>
-          {morePosts.length > 0 && <MoreInfo posts={morePosts} />}
-    </Container>
-    </div>
-
-    
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
+          <div class="w-1/2">
+                <Container>
+                  <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+                  More Stories
+                  </h2>
+                  <div className="grid md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+                    <div class='pr-8 m-1 pl-8 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
+                      <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
+                        <Link  href="https://google.com">
+                          <a className="hover:underline">SHIT</a>
+                        </Link>
+                      </h3>
+                      <p className="text-base leading-relaxed mb-4 text-stone-600">HEHE</p>
+                      {
+                      <button class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100">Learn more</button>
+                      }
+                    </div>
+                  </div>
+                </Container>
+          </div>
   </div>
 
 </div>
 </div>
-    </section>
+</section>
 {/*    BODYYYYY      */}
 
 
