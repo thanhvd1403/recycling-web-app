7629f8b73c1b4a6c1b8fd162ffe67c33a44fc0fb
yenngocduong <yenlinh.2902@gmail.com>
2022-03-27 19:55:12 +0800

    resolve conflict event_page & post_review

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,7 +1,7 @@
-import Avatar from "../components/avatar";
-import DateFormatter from "../components/date-formatter";
-import CoverImage from "./cover-image";
-import Link from "next/link";
+import Avatar from '../components/avatar';
+import DateFormatter from '../components/date-formatter';
+import CoverImage from './cover-image';
+import Link from 'next/link';
 
 export default function PostPreview({
   title,
@@ -12,14 +12,9 @@ export default function PostPreview({
   slug,
 }) {
   return (
-<<<<<<< HEAD
-    <div class='pr-3 m-1 pl-3 m-3 m-2 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
-    {/* <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16"> */}
-=======
-    <div class="pr-8 pl-8 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200">
+    <div class='pr-8 pl-8 pb-8 m-2 bg-gradient-to-b from-white via-white to-emerald-200'>
       {/* <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16"> */}
->>>>>>> 198573cd8aea118026967a6bfcf8686963ef0676
-      <div className="mb-5">
+      <div className='mb-5'>
         <CoverImage
           slug={slug}
           title={title}
@@ -29,18 +24,18 @@ export default function PostPreview({
         />
       </div>
       <h3 className="text-3xl mb-3 align='center' leading-snug text-emerald-800 font-bold">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+        <Link as={`/posts/${slug}`} href='/posts/[slug]'>
+          <a className='hover:underline'>{title}</a>
         </Link>
       </h3>
-      <div className="text-xl mb-4 font-bold">
+      <div className='text-xl mb-4 font-bold'>
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-base leading-relaxed mb-4 text-stone-600">{excerpt}</p>
+      <p className='text-base leading-relaxed mb-4 text-stone-600'>{excerpt}</p>
       {/* <Avatar name={author.name} picture={author.picture} /> */}
       {
         <button
-          class="px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100"
+          class='px-6 py-2 rounded bg-emerald-600 hover:bg-emerald-700 text-green-100'
           onClick={() => (window.location.href = `/posts/${slug}`)}
         >
           Learn more
--- pages/events_page.js
+++ pages/events_page.js
@@ -1,11 +1,11 @@
-import Container from "../components/container";
+import Container from '../components/container';
 // import MoreStories from "../components/more-stories";
-import EventPosts from "../components/event-posts";
-import HeroPost from "../components/hero-post";
-import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
+import EventPosts from '../components/event-posts';
+import HeroPost from '../components/hero-post';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { CMS_NAME } from '../lib/constants';
 
 export default function EventPage({ allPosts }) {
   const heroPost = allPosts[0];
@@ -17,11 +17,11 @@ export default function EventPage({ allPosts }) {
           <title>Events</title>
         </Head>
 
-        <div class="flex flex-wrap flex-grow justify-center">
+        <div class='flex flex-wrap flex-grow justify-center'>
           <img
-            src="/assets/background_event_page2.png"
-            class="w-full h-auto"
-            alt="..."
+            src='/assets/background_event_page2.png'
+            class='w-full h-auto'
+            alt='...'
           />
         </div>
         {/* {heroPost && (
@@ -39,30 +39,30 @@ export default function EventPage({ allPosts }) {
           {morePosts.length > 0 && <EventPosts posts={morePosts} />}
         </Container>
 
-        <div class="pt-6 pb-12 pl-24 pr-24  md:w-2/3 xl:w-auto mx-auto flex flex-col xl:items-stretch justify-between xl:flex-row bg-gradient-to-b from-white via-yellow-100 to-emerald-300">
-          <div class="xl:w-1/2 md:mb-14 xl:mb-0 relative h-auto flex items-center justify-center">
+        <div class='pt-6 pb-12 pl-24 pr-24  md:w-2/3 xl:w-auto mx-auto flex flex-col xl:items-stretch justify-between xl:flex-row bg-gradient-to-b from-white via-yellow-100 to-emerald-300'>
+          <div class='xl:w-1/2 md:mb-14 xl:mb-0 relative h-auto flex items-center justify-center'>
             <img
-              src="/assets/background_event_page.png"
-              alt="Envelope with a newsletter"
-              role="img"
-              class="w-auto h-auto  xl:w-full lg:w-1/2"
+              src='/assets/background_event_page.png'
+              alt='Envelope with a newsletter'
+              role='img'
+              class='w-auto h-auto  xl:w-full lg:w-1/2'
             />
           </div>
-          <div class="w-full xl:w-1/2 xl:pl-10 xl:py-28">
-            <h1 class="text-2xl md:text-4xl xl:text-5xl font-bold leading-10 text-teal-800 dark:text-white mb-4 text-center xl:text-left md:mt-0 mt-4">
+          <div class='w-full xl:w-1/2 xl:pl-10 xl:py-28'>
+            <h1 class='text-2xl md:text-4xl xl:text-5xl font-bold leading-10 text-teal-800 dark:text-white mb-4 text-center xl:text-left md:mt-0 mt-4'>
               Subscribe
             </h1>
-            <p class="text-base leading-normal text-teal-800 dark:text-teal-200 text-center xl:text-left">
+            <p class='text-base leading-normal text-teal-800 dark:text-teal-200 text-center xl:text-left'>
               Subscribe to our mailing list to be the first one receiving
               e-newsletter and events about E-recycling.
             </p>
-            <div class="flex items-stretch mt-12">
+            <div class='flex items-stretch mt-12'>
               <input
-                class="bg-gray-100 rounded-lg rounded-r-none dark:bg-teal-800 text-base leading-none text-gray-800 dark:text-white p-5 w-4/5 border border-transparent focus:outline-none focus:border-gray-500"
-                type="email"
-                placeholder="Your Email"
+                class='bg-gray-100 rounded-lg rounded-r-none dark:bg-teal-800 text-base leading-none text-gray-800 dark:text-white p-5 w-4/5 border border-transparent focus:outline-none focus:border-gray-500'
+                type='email'
+                placeholder='Your Email'
               />
-              <button class="w-32 rounded-l-none hover:bg-emerald-600 bg-emerald-700 rounded text-base font-medium leading-none text-white p-5 uppercase focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700">
+              <button class='flex-auto w-32 rounded-l-none hover:bg-emerald-600 bg-emerald-700 rounded text-base font-medium leading-none text-white p-5 uppercase focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700'>
                 subscribe
               </button>
             </div>
@@ -75,12 +75,12 @@ export default function EventPage({ allPosts }) {
 
 export async function getStaticProps() {
   const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
   ]);
 
   return {
